0f2c2868575df56ef3674f6a7be205fb29dd1be5
qinwenjing <qinwenjing@baidu.com>
2020-04-23 13:22:59 +0800

    clean

--- src/main/java/gittest/simpleTest.java
+++ src/main/java/gittest/simpleTest.java
@@ -2,10 +2,6 @@ package gittest;
 
 public class simpleTest {
     public static void main(String[] args) {
-        System.out.println("test001");
-        System.out.println("test002");
-        System.out.println("test003");
-        System.out.println("test004");
-        System.out.println("test005");
+
     }
 }
