cb9d170193c07908a2c8d144a1361f3a6d3531dd
qinwenjing <qinwenjing@baidu.com>
2020-04-23 14:44:51 +0800

    master000

--- src/main/java/gittest/simpleTest.java
+++ src/main/java/gittest/simpleTest.java
@@ -2,6 +2,8 @@ package gittest;
 
 public class simpleTest {
     public static void main(String[] args) {
+        System.out.println("maste000");
+
         System.out.println("test001");
         System.out.println("test002");
         System.out.println("test003");
