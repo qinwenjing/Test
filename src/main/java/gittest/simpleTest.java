edf4e3fdda4a30ebcb551631f4e7a80188ea6f8a
qinwenjing <qinwenjing@baidu.com>
2020-04-23 13:13:14 +0800

    test001
    test002
    
    test003

---
+++ src/main/java/gittest/simpleTest.java
@@ -0,0 +1,9 @@
+package gittest;
+
+public class simpleTest {
+    public static void main(String[] args) {
+        System.out.println("test001");
+        System.out.println("test002");
+        System.out.println("test003");
+    }
+}
