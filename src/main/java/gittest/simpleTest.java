be9c4b0f982152562febbf701e9ad4c836e5cdeb
qinwenjing <qinwenjing@baidu.com>
2020-04-23 13:29:00 +0800

    合并提交

--- src/main/java/gittest/simpleTest.java
+++ src/main/java/gittest/simpleTest.java
@@ -2,6 +2,10 @@ package gittest;
 
 public class simpleTest {
     public static void main(String[] args) {
+        System.out.println("test001");
+        System.out.println("test002");
+        System.out.println("test003");
+        System.out.println("test004");
 
     }
 }
