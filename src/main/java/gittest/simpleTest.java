92400d08a048e08a8fa97b932eaaef021b8b757b
qinwenjing <qinwenjing@baidu.com>
2020-04-23 14:23:29 +0800

    update

--- src/main/java/gittest/simpleTest.java
+++ src/main/java/gittest/simpleTest.java
@@ -6,25 +6,5 @@ public class simpleTest {
         System.out.println("test002");
         System.out.println("test003");
         System.out.println("test004");
-
-
-        System.out.println("qinwenjing");
-
-        System.out.println("updatedev001");
-        System.out.println("updatedev002");
-
-
-        int mmm = 0;
-
-        System.out.println("master先添加代码");
-
-
-        System.out.println("master最后添加一行代码");
-
-        System.out.println("master111");
-
-        System.out.println("master222");
-
-
     }
 }
